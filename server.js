c6b8e68b81d0355864fc47b67016f59609804148
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2024-08-16 13:39:58 +0200

    created a connection mysql

--- server.js
+++ server.js
@@ -1,9 +1,44 @@
 const express = require("express")
 const server = express()
-// const { connect } = require("http2");
+// const { connect } = require("http2")
 const mysql = require("mysql2") 
-server.use (express.json)
+server.use(express.json())
+
+
+
+const connecting =  mysql.createConnection({ 
+      user: "root",
+      host: "localhost",
+      database: "HOSPITAL_MANAGEMENT",
+      password:"Letsdoit!"
+      
+
+
+    });
+
+
+    
+
+
+server.get("/")((req, res) => {
+  
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n');
+});
 
 
 
 
+
+
+
+  
+    
+
+
+
+ 
+  server.listen(2001, () => {
+    console.log('Listening on 127.0.0.1:2000');
+  });
+  
