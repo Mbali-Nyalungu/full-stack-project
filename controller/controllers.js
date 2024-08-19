cc80d13303f2017dcd73d8b0104d695911566d45
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2024-08-19 09:53:02 +0200

    inserted the put method

--- controller/controllers.js
+++ controller/controllers.js
@@ -55,6 +55,41 @@ exports.createAllDoctors = async (req ,res) =>{
         }
     }
     
+
+    
+    exports.putAllPatientsById = async (req, res) => {
+        const { Patient_num } = req.params;
+        const {Patient_Surname, Patient_name,DateOfBirth ,gender,Patient_cellphone } = req.body;
+    
+        try {
+            
+            const [patient] = await con.promise().query('SELECT * FROM PATIENT_');
+            const Patients = Patients.find(pat => pat.Patient_num  == Patient_num );
+    
+            if (!patient) {
+                return res.status(404).json({ message: "Patient not found" });
+            }
+        
+        const updateQuery = `
+            UPDATE DOCTOR_T
+            SET Patient_Surname = ?, Patient_name= ?, DateOfBirth = ?, gender = ?, Patient_cellphone] = ?, 
+            WHERE Patient_num = ?
+        `;
+        const updateValues = [Patient_num,Patient_Surname, Patient_name,DateOfBirth ,gender,Patient_cellphone];
+
+        const [results] = await con.promise().query(updateQuery, updateValues);
+
+        if (results.affectedRows > 0) {
+            res.status(201).send("Data successfully updated");
+
+        } else {
+            res.status(404).send("Patient not found");
+        }
+    } catch (err) {
+        console.error(`Error executing query: ${err}`);
+        res.status(500).send('An error occurred while updating data');
+    }
+}
      
     
     
