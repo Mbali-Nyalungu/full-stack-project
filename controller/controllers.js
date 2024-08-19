27bb16f3783937fb4dffe140496ff78c7fe68b79
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2024-08-19 09:27:07 +0200

    removed the post method on server.js to controllers.js

--- controller/controllers.js
+++ controller/controllers.js
@@ -1,1 +1,18 @@
 const mysql = require("mysql2") 
+
+
+exports.getAllPatients = async (req, res) =>{
+
+    const query = 'SELECT * FROM PATIENT_';
+
+        try {
+
+            const [results] = await con.promise().query(query);
+            res.json(results);
+            console.log(results);
+        } catch (err) {
+            console.error(`Error executing query: ${err}`);
+            res.status(500).send('An error occurred while retrieving data');
+        }
+
+}
--- server.js
+++ server.js
@@ -33,21 +33,7 @@ const connecting =  mysql.createConnection({
     
 
 
-    exports.getAllPatients = async (req, res) =>{
-
-      const query = 'SELECT * FROM PATIENT_';
-  
-          try {
-  
-              const [results] = await con.promise().query(query);
-              res.json(results);
-              console.log(results);
-          } catch (err) {
-              console.error(`Error executing query: ${err}`);
-              res.status(500).send('An error occurred while retrieving data');
-          }
-  
-  }
+    
 
   exports.createAllDoctors = async (req ,res) =>{ 
     const { Patient_num,Patient_Surname, Patient_name,DateOfBirth ,gender,Patient_cellphone } = req.body;
